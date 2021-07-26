5616ce3cfc4940b0d3cd1df45d94c49899713fe6
romulofgouvea <romulofgouvea@gmail.com>
2021-07-26 09:33:48 -0300

    Corrigindo saida do pdf

--- Frontend/src/Utils/PdfUtils.ts
+++ Frontend/src/Utils/PdfUtils.ts
@@ -10,17 +10,25 @@ import { savePDF } from "@progress/kendo-react-pdf";
 import { Email } from '../models';
 
 const GenerateBase64Pdf = async (element: HTMLElement, options?: DrawOptions) =>
-	await drawDOM(element, options)
+	drawDOM(element, {
+		paperSize: "A4",
+		margin: {
+			left: 40,
+			top: 40,
+			right: 40,
+			bottom: 40,
+		}
+	})
 		.then((group: Group) => exportPDF(group))
 		.then((dataUri: string) => {
 			return dataUri.replace('data:application/pdf;base64,', '');
 		});
 
-const GenerateFilePdfDownload = (element: HTMLElement, filename: string): void => {
+const GenerateFilePdfDownload = (element: HTMLElement, filename: string = 'arquivo'): void => {
 	savePDF(element, {
 		paperSize: "A4",
 		margin: 40,
-		fileName: `${filename}-${new Date().getDay()}-${new Date().getMonth()}-${new Date().getFullYear()}`,
+		fileName: `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDay()}-${filename}.pdf`,
 	});
 }
 
@@ -28,9 +36,9 @@ const SendToAPI = async (email: Email) => {
 	const url = 'http://localhost:7071/api/post-budget';
 	const response = await fetch(url, {
 		method: 'POST',
-		body: JSON.stringify(email),
+		body: JSON.stringify({ email}),
 	});
-	return response.json();
+	return response && response.json();
 }
 
 const PdfUtils = {
